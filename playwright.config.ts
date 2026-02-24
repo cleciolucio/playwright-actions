bd34682df9b82f2393e59b44b43ac72835927580
Cléciolucio Tavares de Pontes <cleciolucio.pontes@bradesco.com.br>
2026-02-24 11:51:25 -0300

    simulando a correção do bug e também testes mult-browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,16 +42,14 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-//
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
